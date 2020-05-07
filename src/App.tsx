e3f8621b246cb170a098e428361e77787a87fc71
Victor Lebrun <victor.lebrun011@gmail.com>
2020-05-07 19:09:49 +0200

    Restore maposaic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import Mapozaic from './Mapozaic/Mapozaic'
-import SvgTest from 'SvgTest/SvgTest'
 
 function App() {
-  // return <Mapozaic />
-  return <SvgTest />
+  return <Mapozaic />
+  // return <SvgTest />
 }
 
 export default App
