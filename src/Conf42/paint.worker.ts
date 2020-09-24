dd48e91f881ddd5ecc2a1208ae7a32d66f3d4cf1
Victor Lebrun <victor.lebrun011@gmail.com>
2020-09-24 18:37:33 +0200

    solution

--- src/Conf42/paint.worker.ts
+++ src/Conf42/paint.worker.ts
@@ -1,11 +1,16 @@
+import { CanvasDataTransformer } from 'Conf42/CanvasDataTransformer'
+import { WorkerPayload } from 'Conf42/types'
+
 const ctx: Worker = self as any
 
 export type WorkerResponse = Uint8ClampedArray
 
 // Respond to message from parent thread
-ctx.onmessage = (ev) => {
-  console.log('worker', ev)
-  ctx.postMessage('gotit')
+ctx.onmessage = ({ data: { sourcePixelArray, targetPixelArray, size } }: { data: WorkerPayload }) => {
+  console.log('worker')
+  const transformer = new CanvasDataTransformer(sourcePixelArray, targetPixelArray, size)
+  transformer.paintTargetData()
+  ctx.postMessage(transformer.targetPixelArray)
 }
 
 export {}
