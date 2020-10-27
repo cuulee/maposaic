07ffea403a7b0afd23eb5acd5abc11fb36f5fa2e
Victor Lebrun <victor.lebrun011@gmail.com>
2020-10-27 08:46:56 +0100

    add react router and gallery page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Mapozaic from 'Mapozaic/Mapozaic'
+import Gallery from 'Gallery/Gallery'
 
 function App() {
-  return <Mapozaic />
+  return (
+    <Router>
+      <Switch>
+        <Route path="/gallery">
+          <Gallery />
+        </Route>
+        <Route path="/">
+          <Mapozaic />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
