19a1750b48f5206b3a515b01d0bab0f3e1adb8b8
Victor Lebrun <victor.lebrun011@gmail.com>
2020-11-08 08:53:13 +0100

    add modal to view pics

--- src/Gallery/Gallery.tsx
+++ src/Gallery/Gallery.tsx
@@ -3,12 +3,9 @@ import React, { useEffect, useState } from 'react'
 
 import 'Gallery/style.less'
 import Title from 'antd/lib/typography/Title'
-import { Button, Divider } from 'antd'
-import { PRIMARY_COLOR } from 'constants/colors'
+import { Divider, Modal } from 'antd'
 
-import spinner from 'assets/spinner.png'
-import { HomeOutlined } from '@ant-design/icons'
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 type ApiPicture = {
   pictureName?: string
@@ -22,9 +19,13 @@ type Picture = {
   downloadURL: string
 }
 
+const displayedName = (name: string | undefined): string => {
+  return name || 'no name'
+}
+
 const Gallery = () => {
   const [pictures, setPictures] = useState<Picture[]>([])
-  const history = useHistory()
+  const [displayedIndex, setDisplayedIndex] = useState<number | null>(null)
 
   const fetchData = async () => {
     try {
@@ -57,15 +58,33 @@ const Gallery = () => {
       <Link to="/">from maposaic</Link>
       <Divider />
       <div className="gallery__pictures">
-        {pictures.map((pic) => {
+        {pictures.map((pic, index) => {
           return (
-            <div onClick={() => window.open(pic.downloadURL)} className="gallery__picture" key={pic.id}>
-              <img className="gallery__picture__image" src={pic.downloadURL} />
-              <div className="gallery__picture__name">{pic.pictureName || 'no name'}</div>
+            <div onClick={() => setDisplayedIndex(index)} className="gallery__picture" key={pic.id}>
+              <img
+                className="gallery__picture__image"
+                alt={`pic-${displayedName(pic.pictureName)}`}
+                src={pic.downloadURL}
+              />
+              <div className="gallery__picture__name">{displayedName(pic.pictureName)}</div>
             </div>
           )
         })}
       </div>
+      <Modal
+        closable={false}
+        visible={displayedIndex !== null}
+        onCancel={() => setDisplayedIndex(null)}
+        bodyStyle={{ height: '68vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      >
+        {displayedIndex !== null && pictures.length > displayedIndex && displayedIndex >= 0 && (
+          <img
+            className="modal__picture"
+            alt={`pic-${displayedName(pictures[displayedIndex].pictureName)}`}
+            src={pictures[displayedIndex].downloadURL}
+          />
+        )}
+      </Modal>
     </div>
   )
 }
