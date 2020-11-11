b9d59ea6c36248855e076f4598880ec33282dd12
Victor Lebrun <victor.lebrun011@gmail.com>
2020-11-11 19:06:32 +0100

    add node-fetch

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions'
+import fetch from 'node-fetch'
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -6,4 +7,13 @@ import * as functions from 'firebase-functions'
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true })
   response.send('Hello from Firebase!')
+})
+
+export const fetch3Geonames = functions.https.onRequest(async (request, response) => {
+  const result = await fetch('https://api.3geonames.org/?randomland=yes')
+
+  const text = await result.text()
+  console.log('coucou', text)
+  console.log('finn dou')
+  response.send(text)
 })
