1a33e2d8056e3228b2ffd6cc47070052f2c5132c
Victor Lebrun <victor.lebrun011@gmail.com>
2020-11-11 22:38:47 +0100

    add cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,8 @@ import * as functions from 'firebase-functions'
 import fetch from 'node-fetch'
 import { parseStringPromise } from 'xml2js'
 
+const cors = require('cors')({ origin: true })
+
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
@@ -10,26 +12,14 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!')
 })
 
-// type GeoData = {
-//   geodata: {
-//     nearest: {
-//       latt: number
-//       longt: number
-//       elevation: number
-//       timezone: string
-//       city: string
-//       name: string
-//       prov: string
-//     }
-//   }
-// }
-
 export const fetch3Geonames = functions.https.onRequest(async (request, response) => {
   try {
     const result = await fetch('https://api.3geonames.org/?randomland=yes')
     const text = await result.text()
     const res = await parseStringPromise(text)
-    response.send(res)
+    cors(request, response, () => {
+      response.send(res)
+    })
   } catch {
     response.status(500).send('Error')
   }
