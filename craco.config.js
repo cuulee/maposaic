dd05b3b351bbc31b0058764217a8635e9cc44837
Victor Lebrun <victor.lebrun011@gmail.com>
2021-01-09 21:25:06 +0100

    add wasm in webpack config

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const path = require('path')
 const CracoLessPlugin = require('craco-less')
 
 module.exports = {
@@ -18,6 +18,29 @@ module.exports = {
     },
   ],
   webpack: {
-    plugins: [new BundleAnalyzerPlugin()],
+    // plugins: [new BundleAnalyzerPlugin()],
+    configure: function override(config, env) {
+      const wasmExtensionRegExp = /\.wasm$/
+
+      config.resolve.extensions.push('.wasm')
+
+      config.module.rules.forEach((rule) => {
+        ;(rule.oneOf || []).forEach((oneOf) => {
+          if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
+            // make file-loader ignore WASM files
+            oneOf.exclude.push(wasmExtensionRegExp)
+          }
+        })
+      })
+
+      // add a dedicated loader for WASM
+      config.module.rules.push({
+        test: wasmExtensionRegExp,
+        include: path.resolve(__dirname, 'src'),
+        use: [{ loader: require.resolve('wasm-loader'), options: {} }],
+      })
+
+      return config
+    },
   },
 }
