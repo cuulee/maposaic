aab4c4c8324df0837222a279aaa8d0d4d0f6c31b
Victor Lebrun <victor.lebrun011@gmail.com>
2021-01-11 09:10:46 +0100

    move game_of_life in other file

--- map-converter/pkg/map_converter.js
+++ map-converter/pkg/map_converter.js
@@ -1,2 +1,3 @@
 import * as wasm from "./map_converter_bg.wasm";
 export * from "./map_converter_bg.js";
+wasm.__wbindgen_start();
--- map-converter/pkg/map_converter_bg.wasm.d.ts
+++ map-converter/pkg/map_converter_bg.wasm.d.ts
@@ -1,6 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
+export function run(): void;
+export function convert(a: number): void;
 export function __wbg_universe_free(a: number): void;
 export function universe_width(a: number): number;
 export function universe_height(a: number): number;
@@ -9,3 +11,4 @@ export function universe_tick(a: number): void;
 export function universe_new(): number;
 export function universe_render(a: number, b: number): void;
 export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_start(): void;
